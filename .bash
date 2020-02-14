8170dd423c3e9c5666a39f8c8e2e7a06e287e981
C.ashim <chapagaiashim058@gmail.com>
2020-02-14 12:56:26 +0000

    First

---
+++ .bash
@@ -0,0 +1,30 @@
+#main
+alias art="php artisan"
+
+# make commands
+alias make:controller="art make:controller"
+alias make:model="art make:model"
+alias make:request="art make:request"
+alias make:migration="art make:migration"
+alias make:middleware="art make:middleware"
+
+# serve
+alias serve="art serve"
+
+# migrate
+alias migrate="art migrate"
+
+# route
+alias route:list="art route:list"
+
+# view all artisan commands
+alias list="art list"
+
+# Create storage symbolic lick
+alias storage:link="art storage:link"
+
+#tinker
+alias tinker="art tinker"
+
+
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Laravel useful commands short version
+
+### 1. Copy .bash file to your project base folder
+### 2. Run command <code>source .bash</code> in your terminal and execute given commands
